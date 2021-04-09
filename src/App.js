847f6ca596d8c85eb45af70c0a1ffc52672ac2a5
Leonardo Redin <leonardoredin@outlook.com>
2021-04-09 17:40:39 -0300

    added pagination

--- src/App.js
+++ src/App.js
@@ -1,7 +1,64 @@
 import * as React from 'react'
 
+const URL = 'https://swapi.dev/api/people'
+
+const span = {
+  padding: '16px'
+}
+
 function App () {
-  return <div>HELLO</div>
+  const [people, setPeople] = React.useState([])
+  const [page, setPage] = React.useState(1)
+  const [next, setNext] = React.useState()
+  const [previous, setPrevious] = React.useState()
+
+  React.useEffect(() => {
+    async function fetchPeople () {
+      const people = await window.fetch(`${URL}`)
+      const peopleJson = await people.json()
+
+      setPeople(peopleJson.results)
+      setNext(peopleJson.next)
+      setPrevious(peopleJson.previous)
+    }
+
+    fetchPeople()
+  }, [])
+
+  const handlePagination = async (type) => {
+    if (type === 'next') {
+      const people = await window.fetch(next)
+      const peopleJson = await people.json()
+
+      setPrevious(peopleJson.previous)
+      setNext(peopleJson.next)
+      setPeople(peopleJson.results)
+      setPage(page + 1)
+    } else {
+      const people = await window.fetch(previous)
+      const peopleJson = await people.json()
+
+      setPrevious(peopleJson.previous)
+      setNext(peopleJson.next)
+      setPeople(peopleJson.results)
+      setPage(page - 1)
+    }
+  }
+
+  return (
+    <div>
+      <div>
+        <button disabled={!previous} onClick={() => handlePagination('prev')}>
+          {'<<'}
+        </button>
+        <span style={span}>{page}</span>
+        <button disabled={!next} onClick={() => handlePagination('next')}>
+          {'>>'}
+        </button>
+      </div>
+      <pre>{JSON.stringify(people, null, 2)}</pre>
+    </div>
+  )
 }
 
 export default App
